4ab14076c5781b58328d6deb39b204e3ffec21da
Alpx <alpxit@gmail.com>
2026-09-01 17:57:14 +0300

    fixed: reverting prev password button do not hide if prev password is absent

--- index.js
+++ index.js
@@ -103,8 +103,7 @@ function prepareControls() {
 
   function fillPasswordInputs(hsd) {
     inpCurrentPassword.val(hsd['currentKey'] ? hsd['currentKey'] : '');
-    if (hsd['previousKey'] && hsd['previousKey'] !== hsd['currentKey'])
-      btnRevertPreviousPassword.removeClass('d-none');
+    btnRevertPreviousPassword[hsd['previousKey'] && hsd['previousKey'] !== hsd['currentKey'] ? 'addClass' : 'removeClass']('d-none');
     inpNewPassword.val(hsd['applyNewKey'] ? hsd['applyNewKey'] : '');
     btnSaveNewPassword[hsd['applyNewKey'] ? 'addClass' : 'removeClass']('bg-danger');
     btnSaveNewPassword[!hsd['applyNewKey'] ? 'addClass' : 'removeClass']('d-none');
